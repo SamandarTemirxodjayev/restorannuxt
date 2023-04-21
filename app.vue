cf8defa818be01e5ac10da5f112574c288061373
SamandarTemirxodjayev <iwtuz166@gmail.com>
2023-04-22 01:04:47 +0500

    continue adding pages

--- app.vue
+++ app.vue
@@ -3,14 +3,13 @@
 </template>
 
 <style>
-/* ... */
-.rotate-enter-active,
-.rotate-leave-active {
-  transition: all 1s;
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.4s;
 }
-.rotate-enter-from,
-.rotate-leave-to {
+.page-enter-from,
+.page-leave-to {
   opacity: 0;
-  transform: rotate3d(1, 1, 1, 15deg);
+  filter: blur(1rem);
 }
 </style>
--- pages/menu/_id.vue
+++ pages/menu/_id.vue
@@ -1,11 +1,47 @@
 <template>
   <div class="">
-    Hello
+
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
+  data () {
+    return {
+      menu: [],
+      menus: [],
+      loading: true
+    }
+  },
+
+  asyncData(context) {
+    const { id } = context.params
+    return { id }
+  },
+  created() {
+    axios
+      .get('https://restoranmenu1.vercel.app/menu/' + this.id)
+      .then((response) => {
+        this.menu = response.data.menu
+        axios.get(`https://restoranmenu1.vercel.app/catalogMenus/${response.data.menu.category_id}`)
+        .then((response) => {
+          this.menus = response.data.menus
+        })
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  },
+  methods:{
+    handleLoad(){
+      const timer = setTimeout(() => {
+        this.loading = false
+      }, 500)
+      return () => clearTimeout(timer)
+    }
+  },
+  
 
 }
 </script>
