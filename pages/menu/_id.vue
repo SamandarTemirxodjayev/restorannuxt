e9beaea605bd65d12ea784c8b6e832e3bf957038
SamandarTemirxodjayev <iwtuz166@gmail.com>
2023-04-22 15:00:08 +0500

    add menu items

--- pages/menu/_id.vue
+++ pages/menu/_id.vue
@@ -1,6 +1,56 @@
 <template>
-  <div class="">
-
+  <div class="flex justify-center items-center h-64" v-if="loading">
+    <div role="status">
+      <svg
+        aria-hidden="true"
+        class="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-purple-600"
+        viewBox="0 0 100 101"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+          fill="currentColor"
+        />
+        <path
+          d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+          fill="currentFill"
+        />
+      </svg>
+      <span class="sr-only">Loading...</span>
+      <img :src="`${menu.photo_url}`" alt="" class="none" @load="handleLoad">
+    </div>
+  </div>
+  <div class="bg-white rounded-lg overflow-hidden" v-else>
+    <div class="fixed bg-white h-12 w-12 text-center items-center justify-center flex rounded-full left-[6px] top-3">
+      <nuxt-link to="/home">
+        <img :src="require(`@/assets/back.svg`)" alt="My SVG File" height="20px" width="20px">
+      </nuxt-link>
+    </div>
+    <img :src="`${menu.photo_url}`" alt="" class="w-full h-[270px]">
+    <div class="p-2">
+        <h1 class="text-3xl font-bold mb-4">{{menu.name}}</h1>
+        <p class="text-lg text-gray-500 mb-4">{{menu.description}}</p>
+        <p class="text-2xl font-bold mb-3">
+          {{Number(menu.price).toLocaleString()}} so'm
+        </p>
+      </div>
+      <div class="text-2xl p-2 font-600">Popular Items</div>
+      <div class="p-4">
+        <div v-for="menuItem in menus" :key="`${menuItem._id}`">
+          <nuxt-link :to="`/menu/${menuItem._id}`">
+            <div v-if="id != menuItem._id">
+              <div class="flex items-center p-4">
+                <img :src="`${menuItem.photo_url}`" alt="" class=" h-24 w-32 object-cover rounded-lg">
+                <div class="ml-4">
+                  <h2 class="text-xl font-bold">{{menuItem.name}}</h2>
+                  <p class="text-gray-500 text-sm">{{Number(menuItem.price).toLocaleString()}} so'm</p>
+                </div>
+              </div>
+            </div>
+          </nuxt-link>
+        </div>
+      </div>
   </div>
 </template>
 
@@ -20,14 +70,13 @@ export default {
     return { id }
   },
   created() {
-    axios
-      .get('https://restoranmenu1.vercel.app/menu/' + this.id)
+    axios.get('https://restoranmenu1.vercel.app/menu/' + this.id)
       .then((response) => {
         this.menu = response.data.menu
         axios.get(`https://restoranmenu1.vercel.app/catalogMenus/${response.data.menu.category_id}`)
-        .then((response) => {
-          this.menus = response.data.menus
-        })
+          .then((response) => {
+            this.menus = response.data.menus
+          })
       })
       .catch((error) => {
         console.log(error)
@@ -35,10 +84,7 @@ export default {
   },
   methods:{
     handleLoad(){
-      const timer = setTimeout(() => {
-        this.loading = false
-      }, 500)
-      return () => clearTimeout(timer)
+      this.loading = false
     }
   },
   
